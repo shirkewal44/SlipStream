379bc8f59f863e2d504b3b4fbb597b0f589bc07d
Samir Hirkewal <samir.hirkewal@icloud.com>
2026-03-05 20:33:36 -0500

    Initial Commit

---
+++ SlipStream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlipStream
+//
+//  Created by Samir Hirkewal on 3/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlipStream/SlipStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlipStreamApp.swift
+//  SlipStream
+//
+//  Created by Samir Hirkewal on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SlipStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
